1cd1be98d30684a7f39805bdb9f832e3f684415d
lizhiming <lizhiming@ksjgs.com>
2018-01-24 18:03:47 +0800

    qqstep start

---
+++ app/src/main/java/lzm/cn/riseskillproject/view/QQStepView.java
@@ -0,0 +1,30 @@
+package lzm.cn.riseskillproject.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by lizhiming on 2018/1/24.
+ */
+
+public class QQStepView extends View {
+    public QQStepView(Context context) {
+        this(context, null);
+    }
+
+    public QQStepView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public QQStepView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+
+
+    }
+
+
+
+}
